9e8e545d76468219b48f5eb587ade9216bbfd846
Kornkritpawit <konsoo@hotmail.com>
2020-08-14 15:01:46 +0700

    config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-east-1_BCANfryJe', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_BCANfryJe', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3ml51s23hnn2ebr0iid7en2esn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
